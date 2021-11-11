0bd3fce16272812e7e65928ec0c283dcc33c4181
rishikanth <r3chandr@ucsd.edu>
2021-11-10 18:55:11 -0800

    build setting changes

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="torch-hd", # Replace with your own username
-    version="0.0.0.1",
+    version="0.0.0.2",
     author="Rishikanth",
     author_email="r3chandr@ucsd.edu",
-    description="Optimized implementations of HD functions using pytorch",
+    description="Optimized implementations of HD functions using pytorch with GPU support",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rishikanthc/torch-hd",
